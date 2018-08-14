53bfb158db04a967f1d0fc2c238ab20fc863a695
Alexandre Machado <alexandre@machado.cc>
2018-08-14 09:50:10 -0300

    setup

---
+++ diacritc-code/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace diacritc_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var usuário = new Usuário { Nome = "João da Silva", Endereço = "São Paulo - SP" };
+            Console.Write($"Nome: {usuário.Nome}, Endereço: {usuário.Endereço}");
+            Console.ReadKey();
+        }
+
+        public class Usuário
+        {
+            public string Nome { get; set; }
+            public string Endereço { get; set; }
+        }
+    }
+}
